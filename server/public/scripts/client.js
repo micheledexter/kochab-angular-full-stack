1c3c43fc31a19dd62a66af0489e77ca814eb9aa8
Shely Dexter <micheleannedexter@gmail.com>
2018-04-26 09:55:04 -0500

    client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,10 @@
 console.log('js');
 
+var app = angular.module('FoodApp', []);
+
+app.controller('FoodController', function () {
+    console.log('FoodController has been loaded');
+    var self = this;
+
+    self.food = 'candy';
+})
